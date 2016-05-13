5412634e20be17dd7dd519924293297f05d8e377
hailuy <304571519@qq.com>
2016-05-13 13:10:51 +0800

    survey

---
+++ strawDiffy/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewController.swift
+//  strawDiffy
+//
+//  Created by itria on 16/5/12.
+//  Copyright © 2016年 Karidon. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    required init?(coder aDecoder:NSCoder ) {
+        super.init(coder: aDecoder)
+    }
+    init () {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
